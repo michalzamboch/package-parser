4f743bbed3b7820737262080956e777ee4122328
Michal Žamboch <zamboch.michal@seznam.cz>
2025-07-27 19:57:59 +0200

    package map

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,38 @@
-use std::collections::HashSet;
+#![allow(dead_code)]
+
+use std::collections::*;
+
+pub fn create_package_map(packages: Vec<&str>) -> HashMap<&str, Vec<&str>> {
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for package in packages {
+        let row = package.split(".").collect::<Vec<&str>>();
+
+        match map.get_mut(row[0]) {
+            Some(value) => value.push(package),
+            None => _ = map.insert(row[0], vec![package]),
+        };
+    }
+
+    map
+}
+
+pub fn create_package_table(packages: Vec<&str>) -> Vec<Vec<&str>> {
+    let mut table: Vec<Vec<&str>> = vec![];
+    
+    for package in packages {
+        let row = package.split(".").collect::<Vec<&str>>();
+        table.push(row);
+    }
+
+    table
+}
 
 pub fn parse_packages(input: &str) -> Vec<&str> {
-    let mut result = filter_all_packages(input).into_iter().collect::<Vec<&str>>();
+    let mut result = filter_all_packages(input)
+        .into_iter()
+        .collect::<Vec<&str>>();
+
     result.sort();
     result
 }
--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
 
 static BASIC_INPUT: &'static str = "
 Project 'ApplicationLayerClient' has the following package references
@@ -49,5 +49,47 @@ mod tests {
         
         let result = parse_packages("    ");
         assert_eq!(result.len(), 0); 
+    }
+
+    #[test]
+    fn test_package_table() {
+        let packages = parse_packages(BASIC_INPUT);
+        let result = create_package_table(packages);
+
+        assert_eq!(result.len(), 4); 
+
+        assert_eq!(result[0].len(), 1); 
+        assert_eq!(result[0][0], "Autofac"); 
+
+        assert_eq!(result[1].len(), 3); 
+        assert_eq!(result[1][0], "Microsoft"); 
+        assert_eq!(result[1][1], "Extensions"); 
+        assert_eq!(result[1][2], "DependencyInjection"); 
+
+        assert_eq!(result[2].len(), 4); 
+        assert_eq!(result[2][0], "Microsoft"); 
+        assert_eq!(result[2][1], "Extensions"); 
+        assert_eq!(result[2][2], "DependencyInjection"); 
+        assert_eq!(result[2][3], "Abstractions"); 
+
+        assert_eq!(result[3].len(), 3); 
+        assert_eq!(result[3][0], "System"); 
+        assert_eq!(result[3][1], "Diagnostics"); 
+        assert_eq!(result[3][2], "DiagnosticSource"); 
+    }
+
+    #[test]
+    fn test_package_map() {
+        let packages = parse_packages(BASIC_INPUT);
+        let map = create_package_map(packages);
+
+        assert_eq!(map.len(), 3); 
+
+        assert_eq!(map["Autofac"][0], "Autofac");
+
+        assert_eq!(map["Microsoft"][0], "Microsoft.Extensions.DependencyInjection");
+        assert_eq!(map["Microsoft"][1], "Microsoft.Extensions.DependencyInjection.Abstractions");
+
+        assert_eq!(map["System"][0], "System.Diagnostics.DiagnosticSource");
     }
 }
